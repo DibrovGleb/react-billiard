20d88b2f16768b22eecee1396c604d0e0ba08705
Dibrov Gleb <g.t.ru@mail.ru>
2024-03-15 17:21:20 +0300

    canvas ball update

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ function App() {
   return (
     <>
     <Canvas/>
-    <div>testetst223232</div>
     </>
   )
 }
--- src/components/canvas.tsx
+++ src/components/canvas.tsx
@@ -1,61 +1,68 @@
 import './styles/canvas.sass';
-import { FC, useEffect } from 'react';
-import { useRef } from 'react';
+import { FC, useEffect, useRef} from 'react';
 
 const Canvas:FC = () => {
-
-   function getRandomColor() {
-      const letters: string = '0123456789ABCDEF';
-      let color: string = '#';
-      for (let i = 0; i < 6; i++) color += letters[Math.floor(Math.random() * 16)]
-      return color;
-   }      
-
-   function createRect ():void {
-      r.push([...Array(4)].map(()=>Math.round(Math.random()*30)+15).concat([...Array(2)].map(()=>Number((Math.random()*1.5).toFixed(2)))).concat(getRandomColor()))
-   }
-   const r: (string | number)[][]  =[],
-         ref = useRef()
-
-
-   useEffect(() => {
-      const canvas = ref.current,
-            pen = canvas.getContext('2d')
-      canvas.width = window.innerWidth/1.5
-      canvas.height = window.innerHeight/2
-
-      const draw = ()=>{
-         //pen.clearRect(0, 0, canvas.width, canvas.height)
-         // !count ? console.log(false) : console.log(true)
-         pen.fillStyle = "rgba(255, 255, 255, 0.1)";
-         pen.fillRect(0, 0, canvas.width, canvas.height);
-         for (let i = 0; i < r.length; i++) {
-            pen.fillStyle = r[i][6]
-            if(r[i][0] > canvas.width-r[i][2] || r[i][0] < 0) r[i][4]=-r[i][4], r[i][6] = getRandomColor()
-            if(r[i][1] > canvas.height-r[i][3] || r[i][1] < 0) r[i][5]=-r[i][5], r[i][6] = getRandomColor()
-            pen.fillRect(r[i][0]+=r[i][4], r[i][1]+=r[i][5], r[i][2], r[i][3])
-         }
-         requestAnimationFrame(draw)
-      }
-      
-      window.addEventListener('resize', ()=> canvas.width = window.innerWidth/1.5,
-      canvas.height = window.innerHeight/2)
-      draw()
-   }, [])
-
-
-   return (<>
-   <canvas ref={ref}/>
-   <div className="btn-container">
-      {/* <button onClick={()=> {ref.current.getContext('2d').clearRect(0, 0, ref.current.width, ref.current.height)}}>Clear</button> */}
-      <button onClick={()=> createRect()}>Add</button>
-      <button onClick={()=> {for (let i = 0; i < 10; i++) 
-         createRect()}}>Add 10</button>
-      <button onClick={()=> r.shift()}>Delete</button>
-      <button onClick={()=> r.length = 0}>Delete all</button>
-   </div>
-   </>
-   )
+    return (
+    <>
+    <CreateCanvas/>
+    <div className="btn-container">
+        {/* <button onClick={()=> {ref.current.getContext('2d').clearRect(0, 0, ref.current.width, ref.current.height)}}>Clear</button> */}
+        <button>Add</button>
+        <button>Add 10</button>
+        <button>Delete</button>
+        <button>Delete all</button>
+    </div>
+    </>
+    )
 }
 
- export default Canvas
+const CreateCanvas:FC = () =>{
+    const ref = useRef<HTMLCanvasElement | null>(null),
+          circle = { x: 100, y: 100, radius: 20, vx: 0, vy: 0};
+
+    useEffect(()=>{
+        const canvas = ref.current
+        if (!canvas) return alert('canvas not found')
+        canvas.width = window.innerWidth/1.1
+        canvas.height = window.innerHeight/2.5
+        
+        const pen = canvas.getContext('2d')
+        if (!pen) return alert('pen not found')
+        const draw = () => {
+
+            pen.clearRect(0, 0, canvas.width, canvas.height)
+            pen.beginPath()
+            circle.x+circle.radius > canvas.width || circle.x-circle.radius < 0 
+            ? circle.vx = - circle.vx : circle.vx = +circle.vx
+            circle.y + circle.radius > canvas.height || circle.y-circle.radius < 0
+            ? circle.vy = - circle.vy : circle.vy = +circle.vy
+            pen.arc(circle.x+=circle.vx, circle.y+=circle.vy, circle.radius, 0, 2 * Math.PI)
+            circle.vx > 0 ? circle.vx -= 0.005 : circle.vx+=0.005
+            circle.vy > 0 ? circle.vy -= 0.005 : circle.vy+=0.005
+            pen.fillStyle = 'blue'
+            pen.fill()
+      
+            requestAnimationFrame(draw);
+          };
+      
+          draw();
+      
+          const onMouseMove = (event: MouseEvent) => {
+            const dx = event.clientX-23;
+            const dy = event.clientY-10;
+            console.log(dx,dy,Math.trunc(circle.x),Math.trunc(circle.y))
+            if (dx == Math.trunc(circle.x) || dy == Math.trunc(circle.y)){
+                circle.vx = 2, circle.vy = 2;
+            }
+          };
+      
+          window.addEventListener('mousemove', onMouseMove);
+
+    })
+    
+    return (
+        <canvas ref={ref}/>
+    )
+}
+
+export default Canvas
